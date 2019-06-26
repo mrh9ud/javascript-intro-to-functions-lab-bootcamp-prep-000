c2683b16b41d3f050e99cfee4b450ecec57f16a2
Matthew Heavner <mrh9ud@virginia.edu>
2019-06-26 22:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,4 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (${string} === string.toLowerCase()) {
-    return 'I can\'t hear you!'
-  } 
-  else if (${string} === string.toUpperCase()) {
-    return 'YES INDEED!'
-  }
-  else (${string} === "I love you, Grandma.") {
-    return 'I love you, too.'
-  }
-}
+  if (string === string.toLowerCase)
